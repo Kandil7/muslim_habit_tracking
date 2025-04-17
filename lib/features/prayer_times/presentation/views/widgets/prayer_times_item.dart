637660bf521e240d51848fb40bc748de78f1e1ca
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2025-04-17 13:29:11 +0200

    Enhance PrayerTimesItem with animations and better visuals

--- lib/features/prayer_times/presentation/views/widgets/prayer_times_item.dart
+++ lib/features/prayer_times/presentation/views/widgets/prayer_times_item.dart
@@ -5,38 +5,112 @@ import '../../../data/models/prayer_item_model.dart';
 import 'prayer_times_item_title.dart';
 import 'prayer_times_item_trailing.dart';
 
-class PrayerTimesItem extends StatelessWidget {
+class PrayerTimesItem extends StatefulWidget {
   const PrayerTimesItem(
       {super.key, required this.prayerItemModel, required this.isNextPrayer});
   final PrayerItemModel prayerItemModel;
   final bool isNextPrayer;
 
   @override
+  State<PrayerTimesItem> createState() => _PrayerTimesItemState();
+}
+
+class _PrayerTimesItemState extends State<PrayerTimesItem> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.05,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    _opacityAnimation = Tween<double>(
+      begin: 1.0,
+      end: 0.8,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    if (widget.isNextPrayer) {
+      _controller.repeat(reverse: true);
+    }
+  }
+
+  @override
+  void didUpdateWidget(PrayerTimesItem oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isNextPrayer != oldWidget.isNextPrayer) {
+      if (widget.isNextPrayer) {
+        _controller.repeat(reverse: true);
+      } else {
+        _controller.stop();
+        _controller.reset();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 54,
-        width: double.infinity,
-        margin: EdgeInsets.symmetric(horizontal: 4),
-        decoration: BoxDecoration(
-          color: isNextPrayer ? AppColors.primaryColor2 : AppColors.whiteColor,
-          borderRadius: BorderRadius.circular(4),
-          boxShadow: [
-            BoxShadow(
-                blurRadius: isNextPrayer ? 0 : 15,
-                color: isNextPrayer
-                    ? Colors.transparent
-                    : AppColors.blackColor.withValues(alpha: .15))
-          ],
-        ),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      height: 64,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: widget.isNextPrayer ? AppColors.primaryColor2 : AppColors.whiteColor,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: widget.isNextPrayer ? 8 : 4,
+            spreadRadius: widget.isNextPrayer ? 2 : 0,
+            color: widget.isNextPrayer
+                ? AppColors.primaryColor2.withOpacity(0.3)
+                : AppColors.blackColor.withValues(alpha: .1),
+          )
+        ],
+      ),
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: widget.isNextPrayer ? _scaleAnimation.value : 1.0,
+            child: Opacity(
+              opacity: widget.isNextPrayer ? _opacityAnimation.value : 1.0,
+              child: child,
+            ),
+          );
+        },
         child: Row(
           children: [
-            const SizedBox(width: 10),
+            const SizedBox(width: 12),
             PrayerTimesItemTitle(
-                prayerItemModel: prayerItemModel, isNextPrayer: isNextPrayer),
+                prayerItemModel: widget.prayerItemModel, isNextPrayer: widget.isNextPrayer),
             PrayerTimesItemTrailing(
-                prayerItemModel: prayerItemModel, isNextPrayer: isNextPrayer),
-            const SizedBox(width: 10),
+                prayerItemModel: widget.prayerItemModel, isNextPrayer: widget.isNextPrayer),
+            const SizedBox(width: 12),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
