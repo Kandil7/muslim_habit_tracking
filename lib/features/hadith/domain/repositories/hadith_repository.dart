7b03948a17809a644054369189929a654d72d7ef
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2025-04-17 20:54:14 +0200

    Step 3: Create Hadith repository interface

---
+++ lib/features/hadith/domain/repositories/hadith_repository.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../entities/hadith.dart';
+
+/// Repository interface for Hadith feature
+abstract class HadithRepository {
+  /// Get a list of all hadiths
+  Future<Either<Failure, List<Hadith>>> getAllHadiths();
+
+  /// Get a hadith by its ID
+  Future<Either<Failure, Hadith>> getHadithById(String id);
+
+  /// Get the hadith of the day
+  Future<Either<Failure, Hadith>> getHadithOfTheDay();
+
+  /// Get hadiths by source (e.g., Bukhari, Muslim)
+  Future<Either<Failure, List<Hadith>>> getHadithsBySource(String source);
+
+  /// Get hadiths by tag or category
+  Future<Either<Failure, List<Hadith>>> getHadithsByTag(String tag);
+
+  /// Get bookmarked hadiths
+  Future<Either<Failure, List<Hadith>>> getBookmarkedHadiths();
+
+  /// Toggle bookmark status for a hadith
+  Future<Either<Failure, Hadith>> toggleHadithBookmark(String id);
+}
