4e7ef6d7ce069415ba7e6f98da0ac986eb249a0d
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2025-04-17 14:09:06 +0200

    Add error handling for location services

--- lib/features/prayer_times/presentation/views/widgets/prayer_view_body.dart
+++ lib/features/prayer_times/presentation/views/widgets/prayer_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:muslim_habbit/features/prayer_times/presentation/manager/prayer/prayer_cubit.dart';
 import 'package:muslim_habbit/features/prayer_times/presentation/views/widgets/prayer_view_header_no_ads.dart';
 
+import 'location_error_widget.dart';
 import 'prayer_date_hijri_and_milad.dart';
 import 'prayer_times_header.dart';
 import 'prayer_times_list_view.dart';
@@ -11,22 +14,30 @@ class PrayerViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: Column(
-          children: [
-            const PrayerViewHeaderNoAds(),
-            const SizedBox(height: 12),
-            const PrayerTimesHeader(),
-            const SizedBox(height: 4),
-            const PrayerDateHijriAndMilad(),
-            const SizedBox(height: 20),
-            const PrayerTimesListView()
-          ],
-        ),
-      ),
+    return BlocBuilder<PrayerCubit, PrayerState>(
+      builder: (context, state) {
+        return SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              children: [
+                const PrayerViewHeaderNoAds(),
+                const SizedBox(height: 12),
+                if (state is SetLocationError)
+                  LocationErrorWidget(errorMessage: state.message),
+                if (state is GetPrayerError)
+                  LocationErrorWidget(errorMessage: state.message),
+                const PrayerTimesHeader(),
+                const SizedBox(height: 4),
+                const PrayerDateHijriAndMilad(),
+                const SizedBox(height: 20),
+                const PrayerTimesListView()
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
