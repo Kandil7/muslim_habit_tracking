8b91af4784d4d477e3f0c111d8ecc2956dd41233
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2025-04-17 18:48:42 +0200

    Update desugar_jdk_libs version to 2.1.4 to fix dependency conflict with flutter_local_notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,36 +6,29 @@ plugins {
 }
 
 android {
-    namespace = "com.ramadanhabit.ramadan_habit_tracking"
+    namespace = "com.ramadanhabit.muslim_habbit"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // Updated NDK version for plugin compatibility
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-        // Enable core library desugaring for flutter_local_notifications
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ramadanhabit.ramadan_habit_tracking"
+        applicationId = "com.ramadanhabit.muslim_habbit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23 // Updated minSdk for plugin compatibility
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        // Enable multidex support
-        multiDexEnabled = true
-
-        // Set vector drawables support
-        vectorDrawables.useSupportLibrary = true
     }
 
     buildTypes {
@@ -43,19 +36,6 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-
-            // Enable minification and optimization
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-
-        debug {
-            // Disable minification for debug builds for faster build times
-            isMinifyEnabled = false
-            isShrinkResources = false
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
         }
     }
 }
@@ -65,13 +45,8 @@ flutter {
 }
 
 dependencies {
-    // Add the desugaring dependency for Java 8 features
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
     // Add multidex support
     implementation("androidx.multidex:multidex:2.0.1")
-
-    // Add core androidx libraries
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
 }
